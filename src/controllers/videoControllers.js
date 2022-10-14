a44a1579ee16688321cbaf1d74fdbc07a54b24f8
Hank <dlwkdans@korea.ac.kr>
2022-10-14 17:12:48 +0900

    20221014 퇴근전커밋

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,10 +1,7 @@
-const fakeUser = {
-  userName: "dlwkdans",
-  loggedIn: true,
+export const handleHome = (req, res) => {
+  const videos = [];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-
-export const handleHome = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser: fakeUser });
 // export const handleVideo = (req, res) => res.send("Video!");
 export const search = (req, res) => res.send("Search!");
 export const upload = (req, res) => res.send("Upload!");
