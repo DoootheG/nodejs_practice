fc1b728d22bfe5c63b63d2156b07f40bf43c0d2f
Hank <dlwkdans@korea.ac.kr>
2022-11-21 17:32:01 +0900

    20221121

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,6 +1,7 @@
+export const join = (req, res) => res.render("creatAccount");
+
 export const logout = (req, res) => res.send("Log out!");
 export const edit = (req, res) => res.send("Edit!");
 export const remove = (req, res) => res.send("Remove!");
 export const id = (req, res) => res.send("ID!");
-export const join = (req, res) => res.send("Join!");
 export const login = (req, res) => res.send("Log in!");
