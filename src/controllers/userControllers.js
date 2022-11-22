621aa0225d9696364e33deeb90c88ef74fa092a4
Hank <dlwkdans@korea.ac.kr>
2022-11-22 16:52:27 +0900

    20221122

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,5 +1,26 @@
-export const join = (req, res) => res.render("creatAccount");
+import User from "../models/User";
 
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = async (req, res) => {
+  console.log(req.body);
+  const { name, username, email, password, location } = req.body;
+  const pageTitle = "Join";
+  const usernameExists = await User.exists({ username });
+  if (usernameExists) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "This username is already taken.",
+    });
+  }
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  res.redirect("/login");
+};
 export const logout = (req, res) => res.send("Log out!");
 export const edit = (req, res) => res.send("Edit!");
 export const remove = (req, res) => res.send("Remove!");
