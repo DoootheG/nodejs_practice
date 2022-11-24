67a61e237e9797a07cbca855ecdff63d325d62d8
Hank <dlwkdans@korea.ac.kr>
2022-11-24 17:28:53 +0900

    20221124

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,10 +1,11 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
-  const { name, username, email, password1, password2, location } = req.body;
+  const { name, username, email, password, password2, location } = req.body;
   const pageTitle = "Join";
-  if (password1 !== password2) {
+  if (password !== password2) {
     return res.status(400).render("join", {
       pageTitle,
       errorMessage: "Password confirmation does not match.",
@@ -26,8 +27,32 @@ export const postJoin = async (req, res) => {
   });
   res.redirect("/login");
 };
+export const getLogin = (req, res) => {
+  return res.render("login", { pageTitle: "Log-in" });
+};
+
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const pageTitle = "Log-in";
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "There is no same username that you submit.",
+    });
+  }
+
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "Wrong password",
+    });
+  }
+  res.end();
+};
+
 export const logout = (req, res) => res.send("Log out!");
 export const edit = (req, res) => res.send("Edit!");
 export const remove = (req, res) => res.send("Remove!");
 export const id = (req, res) => res.send("ID!");
-export const login = (req, res) => res.send("Log in!");
