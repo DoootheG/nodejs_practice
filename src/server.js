1efafce48a260745fed8c6f213706eb4ed03c742
Hank <dlwkdans@korea.ac.kr>
2022-12-14 17:22:05 +0900

    123

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express, { Router } from "express";
 import morgan from "morgan";
 import session from "express-session";
-import Mongostroe from "connect-mongo";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -16,10 +16,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: "Hello!",
-    resave: true,
-    saveUninitialized: true,
-    store: Mongostroe.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
